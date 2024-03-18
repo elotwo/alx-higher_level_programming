0eb8a7b691b3b433771aeb79ba0fd56e1e16b8d5
elotwo <obiagwu94@gmail.com>
2024-03-19 00:40:13 +0100

    na so o

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    elif idx > 5:
+    if idx < 0 or idx > 4:
         return None
     else:
         return my_list[idx]
