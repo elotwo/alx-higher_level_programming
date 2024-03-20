a75c09351372af96aad9aedbf756b5837ce4fb20
elotwo <obiagwu94@gmail.com>
2024-03-20 12:03:45 +0100

    odogwu is commited

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import calculator_1
+a = 10
+b = 5
+result1 = calculator_1.add(a, b)
+result2 = calculator_1.sub(a, b)
+result3 = calculator_1.mul(a, b)
+result4 = calculator_1.div(a, b)
+print(f"{a} + {b} = {result1}")
+print(f"{a} - {b} = {result2}")
+print(f"{a} * {b} = {result3}")
+print(f"{a} / {b} = {result4}")
