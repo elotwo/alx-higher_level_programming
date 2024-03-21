81aa5b65360d3e5e139e96940b60931d74933cbe
elotwo <obiagwu94@gmail.com>
2024-03-21 22:41:40 +0100

    done

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix):
+def square_matrix_simple(matrix=[]):
     squared_matrix = map(lambda row: list(map(lambda x: x*x, row)), matrix)
     squared_matrix_str = str([list(row) for row in squared_matrix])
     print(squared_matrix_str[1:-1])
