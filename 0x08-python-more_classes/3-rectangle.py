db16144c13d7ac94ccd4777b8e2accbb7c16a55e
elotwo <obiagwu94@gmail.com>
2024-04-10 00:31:34 +0100

    m4

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -18,12 +18,15 @@ class Rectangle:
             raise TypeError("height must be an inter")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__height = value 
+        self.__height = value
     def area(self):
-        a = self.width * self.height 
+        a = self.width * self.height
         return a
     def perimeter(self):
         if self.width == 0 or self.height == 0:
-            return 0
+            return ' '
         else:
+            c = str("#")
+            print("{}".format(c))
             return 2 * (self.width + self.height)
+
