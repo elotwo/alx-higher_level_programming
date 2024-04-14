15447340e9500e05e0274b549daa58e2f33841cf
elotwo <obiagwu94@gmail.com>
2024-04-14 16:14:59 +0100

    no be only me dey  here 0

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
-    if isinstance(matrix, (int, float)):
+    divided_results = []
+    list(divided_results)
+    if all(isinstance(x, (int, float))for x in matrix):
         n = len(matrix)
         c = 0
         while c <= n:
@@ -12,14 +14,15 @@ def matrix_divided(matrix, div):
         else:
             raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
         if not isinstance(div, (int, float)):
-            raise TypeErro("div must be a number")
+            raise TypeError("div must be a number")
         if div == 0:
             raise ZeroDivisionError("division by zero")
-        divided_results = []
         for row in matrix:
             divided_row = []
+            list(divided_row)
             for element in row:
                 divide = element / div
                 divided_row.append(divide)
-                divided_results.append(divided_row)
-        return divided_results
+            divided_results.append(divided_row)
+    
+    return divided_results
