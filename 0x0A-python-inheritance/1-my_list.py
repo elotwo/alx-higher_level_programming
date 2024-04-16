0fc1eb7494093fddbbc5df20456f4654b144472a
elotwo <obiagwu94@gmail.com>
2024-04-16 23:50:55 +0100

    ok

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-class list():
-    def __init__(self, append):
-        self.append =   
 class MyList(list):
-    def __init__(self, append):
-        super().__init__(self, append)
-        self.append = append
-    def append(self,li):
-        if isinstance(li, int):
-            super().append(li)
     def print_sorted(self):
-            return sorted(self)
+        sorted_list = sorted(self)
+        print(sorted_list)
