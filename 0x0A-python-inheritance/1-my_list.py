108151d3ccc3ea2a35f6ce2a9879ef9e4d5ae2aa
elotwo <obiagwu94@gmail.com>
2024-04-16 18:51:04 +0100

    never say never

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-class Mylist(list):
-
+class list():
+    def __init__(self, append):
+        self.append =   
+class MyList(list):
+    def __init__(self, append):
+        super().__init__(self, append)
+        self.append = append
+    def append(self,li):
+        if isinstance(li, int):
+            super().append(li)
+    def print_sorted(self):
+            return sorted(self)
