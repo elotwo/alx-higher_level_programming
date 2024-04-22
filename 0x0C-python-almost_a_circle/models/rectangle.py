23a10df8ee9a6fc200262eb54673d0a31f510939
elotwo <obiagwu94@gmail.com>
2024-04-22 15:56:29 +0100

    me

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -39,3 +39,10 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+    def area(self):
+        return self.__height * self.__width
+    def display(self):
+        for _ in range(self.__y):
+            for _ in range(self.__x):
+                print("#",end = "")
+            print()
