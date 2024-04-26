d4862a31c407ebab74eb8bed24c1211a5d3595d7
elotwo <obiagwu94@gmail.com>
2024-04-27 00:05:07 +0100

    me

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,7 +5,7 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-    def size(seif):
+    def size(self):
         return size.width
     def size(self, value):
         self.__width = value
