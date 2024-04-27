7691f973cd29eeddeb1276dcc655bc5262c63f3b
elotwo <obiagwu94@gmail.com>
2024-04-27 14:10:26 +0100

    me

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('/home/emmanuel/alx-higher_level_programming/0x0C-python-almost_a_circle/tests')                        import unittest                                                                                                         from models.rectangle import Rectangle
+class Testrectangle(unittest.TestCase):
+    def test_recangle(self):
+        seif.rect = Rectangle()
+    def sructure(self):
+        self.result = self.rect.display(5,9)
+        self.assertEqual(result.width, 5)
+        self.assertEqual(result.height, 9)
+
