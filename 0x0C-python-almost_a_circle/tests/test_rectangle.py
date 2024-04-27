4ea956709f9c7b7d846e2c1ff2d548ba629acd62
elotwo <obiagwu94@gmail.com>
2024-04-28 00:58:31 +0100

    me

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -5,8 +5,8 @@ sys.path.append('/home/emmanuel/alx-higher_level_programming/0x0C-python-almost_
 class Testrectangle(unittest.TestCase):
     def test_constructor(self):
         rect = Rectangle(5,9)
-        self.assertEqual(rect.width, 5)
-        self.assertEqual(rect.height, 9)
+        self.assertEqual(rect.get_width(), 5)
+        self.assertEqual(rect.get_height(), 9)
     def test_display(self):
         rect = Rectangle(5, 9)
         result = rect.display()
