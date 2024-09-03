6be1657711a246bd2c546e8c24cc87f953fd8d86
elotwo <obiagwu94@gmail.com>
2024-09-03 14:21:04 +0100

    me

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,6 @@
-SHOW GRANTS FOR 'user_0d_1'@'DESKTOP-O8S1R1H';
-SHOW GRANTS FOR 'user_0d_2'@'DESKTOP-O8S1R1H';
+-- This script displays all privileges granted to user user_0d_1 and user_0d_2
+-- this shows the privileges of grants user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- this show the privileges of user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
