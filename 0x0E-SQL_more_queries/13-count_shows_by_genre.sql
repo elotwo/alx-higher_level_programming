82bef46d0497f1a4f00118c6812dbd0ed762ac00
elotwo <obiagwu94@gmail.com>
2024-09-11 12:30:54 +0100

    me

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,1 +1,8 @@
-SELECT tv_genres.name, 
+-- This script display the types of genre and the number of tv shows of that genre
+-- This statement select the genre and the number of shows with the type of genre using the count function
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+HAVING COUNT(tv_show_genres.show_id) > 0
+ORDER BY number_of_shows DESC;
